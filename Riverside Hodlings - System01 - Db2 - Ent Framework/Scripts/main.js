3a7609730ae25b9c5c8e3b82456b1bb31137d6d5
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-11-13 01:59:42 +0200

    latest styles updated

---
+++ Riverside Hodlings - System01 - Db2 - Ent Framework/Scripts/main.js
@@ -0,0 +1,13 @@
+$('#sidebar').toggleClass('active');
+(function ($) {
+    "use strict"; var fullHeight = function () {
+        $('.js-fullheight').css('height',
+            $(window).height()); $(window).resize(function () {
+                $('.js-fullheight').css('height',
+                    $(window).height());
+            });
+    }; fullHeight();
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+})(jQuery);
