cbfa1f5e31fbb75c783bb9dc920dd7d337fd58db
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-11-12 21:56:33 +0200

    updates

--- Riverside Hodlings - System01 - Db2 - Ent Framework/Controllers/HomeController.cs
+++ Riverside Hodlings - System01 - Db2 - Ent Framework/Controllers/HomeController.cs
@@ -4,30 +4,64 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Riverside_Hodlings___System01___Db2___Ent_Framework.Models;
+using PagedList;
 
 namespace Riverside_Hodlings___System01___Db2___Ent_Framework.Controllers
 {
     public class HomeController : Controller
     {
-        private Riverside_HoldingsEntities db = new Riverside_HoldingsEntities();
+        private Riverside_HoldingsEntities1 db = new Riverside_HoldingsEntities1();
         public ActionResult Index()
         {
-           
-            return View();
+            var info = new DashboardVM();
+            info.Admins = db.ADMINISTRATORS.ToList();
+            info.Clients = db.CLIENTS.ToList();
+            info.Invoices = db.INVOICES.ToList();
+            return View(info);
         }
 
-        public ActionResult About()
+        public ActionResult Clients(string searchString, string sortOrder, string currentFilter, int? page)
         {
-            ViewBag.Message = "Your application description page.";
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
 
-            return View();
+            ViewBag.CurrentSort = sortOrder;
+
+            //
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            var clients = from c in db.CLIENTS select c;
+
+            //return results according to search
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                clients = db.CLIENTS.Where(c => c.NAME.Contains(searchString));
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            return View(clients.ToList().ToPagedList(pageNumber, pageSize));
         }
 
-        public ActionResult Contact()
+        public ActionResult Finance()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        public ActionResult Invoices()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
     }
 }
