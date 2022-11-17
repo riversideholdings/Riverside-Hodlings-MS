95e9b4b4ba174508a152f8a8a547fded7d31e256
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-11-17 23:13:33 +0200

    update v1.0.0

--- Riverside Hodlings - System01 - Db2 - Ent Framework/customscript/main.js
+++ Riverside Hodlings - System01 - Db2 - Ent Framework/customscript/main.js
@@ -1,4 +1,9 @@
-﻿$('#sidebar').toggleClass('active');
+﻿$(document).ready(function () {
+    console.log("ready!");
+    $('#sidebar').toggleClass('active');
+});
+
+$('#sidebar').toggleClass('active');
 (function ($) {
     "use strict"; var fullHeight = function () {
         $('.js-fullheight').css('height',
