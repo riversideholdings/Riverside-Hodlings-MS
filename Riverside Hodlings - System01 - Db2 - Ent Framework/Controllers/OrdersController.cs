7da2faa073d24965bc8e17fe4867ea177a74c7e1
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-11-18 00:22:43 +0200

    updates

---
+++ Riverside Hodlings - System01 - Db2 - Ent Framework/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Riverside_Hodlings___System01___Db2___Ent_Framework.Models;
+using PagedList;
+
+namespace Riverside_Hodlings___System01___Db2___Ent_Framework.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private Riverside_HoldingsEntities1 db = new Riverside_HoldingsEntities1();
+        public ActionResult AllOrders(string searchString, string sortOrder, string currentFilter, int? page)
+        {
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            ViewBag.CurrentSort = sortOrder;
+
+            //
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            var invoices = from c in db.INVOICES select c;
+
+            //return results according to search
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                invoices = db.INVOICES.Where(i => i.INVOICE_NUM.Contains(searchString));
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            return View();
+        }
+    }
+}
